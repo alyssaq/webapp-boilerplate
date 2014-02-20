91b3180c944dd8f7336db6e4d004184aaf434929
alyssaq <alyssaquek@gmail.com>
2014-02-20 23:38:32 +0800

    Added replace build blocks in HTML

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
   clean = require('gulp-clean'),
   concat = require('gulp-concat'),
   connect = require('gulp-connect'),
+  gutil = require('gulp-util'),
   htmlreplace = require('gulp-html-replace'),
   imagemin = require('gulp-imagemin'),
   jshint = require('gulp-jshint'),
@@ -16,8 +17,10 @@ var gulp = require('gulp'),
   rename = require('gulp-rename'),
   sass = require('gulp-ruby-sass'),
   size = require('gulp-size'),
-  uglify = require('gulp-uglify'),
-  CFG = {src: 'src/', prod: 'app/'};
+  uglify = require('gulp-uglify');
+
+//Config for source and production folder
+var  CFG = {src: 'src/', prod: 'app/', isProduction: true};
 
 function handleError(err) {
   notify.onError('<%= err.message %>')()
@@ -29,15 +32,15 @@ function handleChange(event) {
   console.log(event.type + ' ' + event.path);
 }
 
-// Styles
+// Compile scss to css, minify and copy
 gulp.task('styles', function() {
   return pipe(
     gulp.src(CFG.src + 'styles/main.scss'),
     sass({
-      style: 'expanded',
+      style: CFG.isProduction ? 'compressed' : 'expanded',
       loadPath: [CFG.src + 'bower_components']
     }),
-    minifycss(),
+    CFG.isProduction ? minifycss() : gutil.noop(),
     rename({suffix: '.min'}),
     autoprefixer('last 1 version'),
     size(),
@@ -47,38 +50,53 @@ gulp.task('styles', function() {
    .on('error', notify.onError('<%= error.message %>'));
 });
 
-// Scripts
+// Minify and copy all Javascript files (except vendor)
 gulp.task('scripts', function () {
   return pipe(
     gulp.src(CFG.src + 'scripts/**/*.js'),
     jshint('.jshintrc'),
     jshint.reporter('default'),
-    concat('main.js'),
-    rename({suffix: '.min'}),
-    uglify(),
+    concat('main.min.js'),
+    CFG.isProduction ? uglify() : gutil.noop(),
     connect.reload(),
     gulp.dest(CFG.prod + 'scripts')
   ).on('error', handleError);
 });
 
-// HTML
+// Copy vendor scripts
+gulp.task('vendorscripts', function () {
+  pipe(
+    gulp.src(CFG.src + 'bower_components/jquery/jquery.min.js'),
+    gulp.dest(CFG.prod + 'scripts/vendor')
+  ).on('error', handleError);
+
+  var base = CFG.src + 'bower_components/bootstrap-sass/vendor/assets/javascripts/bootstrap'
+  pipe(
+    gulp.src([base + '/tooltip.js', base + '/button.js']),
+    concat('bootstrap.min.js'),
+    uglify(),
+    gulp.dest(CFG.prod + 'scripts/vendor')
+  ).on('error', handleError);
+});
+
+// Copy HTML files
 gulp.task('html', function () {
   return pipe(
-    gulp.src(CFG.src + '*.html'),
+    gulp.src(CFG.src + '404.html'),
     connect.reload(),
     size(),
     gulp.dest(CFG.prod)
   ).on('error', handleError);
 });
 
-// Images
+// Compress and copy static images
 gulp.task('images', function () {
   return pipe(
     gulp.src(CFG.src + 'images/**/*'),
     cache(imagemin({
-        optimizationLevel: 3,
-        progressive: true,
-        interlaced: true
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
     })),
     connect.reload(),
     size(),
@@ -86,19 +104,20 @@ gulp.task('images', function () {
   ).on('error', handleError);
 });
 
-// Replace html tags during build
+// Replace build blocks in HTML to scripts or styles
 gulp.task('replace', function() {
   return pipe(
     gulp.src(CFG.src + 'index.html'),
     htmlreplace({
-        'css': 'styles/main.min.css',
-        'js': 'scripts/main.min.js'
+      'css': 'styles/main.min.css',
+      'scripts': 'scripts/main.min.js',
+      'vendorscripts': ['scripts/vendor/jquery.min.js', 'scripts/vendor/bootstrap.min.js']
     }),
     gulp.dest(CFG.prod)
   ).on('error', handleError); 
 });
 
-// Clean
+// Delete files
 gulp.task('clean', function () {
   return pipe(
     gulp.src([CFG.prod + 'styles', 
@@ -110,7 +129,7 @@ gulp.task('clean', function () {
   ).on('error', handleError);
 });
 
-gulp.task('connect', connect.server({
+gulp.task('connect', ['build'], connect.server({
   root: [CFG.prod],
   port: 1337,
   livereload: false,
@@ -123,7 +142,7 @@ gulp.task('connect', connect.server({
 // Watch
 gulp.task('watch', function () {
     // Watch .html files
-    gulp.watch(CFG.src + '*.html', ['html'])
+    gulp.watch(CFG.src + '*.html', ['html', 'replace'])
     .on('change', handleChange)
 
     // Watch .scss files
@@ -140,18 +159,21 @@ gulp.task('watch', function () {
 });
 
 // Build
-gulp.task('build', ['styles', 'scripts', 'images', 'replace']);
-
-// Default task
-gulp.task('default', ['clean'], function () {
-    gulp.start('build');
-});
+gulp.task('build', ['html', 'vendorscripts', 'styles', 'scripts', 'images', 'replace']);
 
 // Just watching style changes
 gulp.task('watchstyles', ['styles', 'connect', 'watch'])
 
-// dev task
-gulp.task('dev', ['clean', 'build'], function () {
+// Default task: Production mode
+gulp.task('default', ['clean'], function () {
+  CFG.isProduction = true;
+  gulp.start('build');
+});
+
+// Task: Development mode
+gulp.task('dev', ['clean'], function () {
+  CFG.isProduction = false;
+  gulp.start('build')
   gulp.start('connect');
   gulp.start('watch');
 });
